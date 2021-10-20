2d8fd4ee2361abf2d1f0b5b5ba15619bf00f8c66
Kimhyeri <hyelljs@gmail.com>
2021-10-20 10:09:35 +0900

    test

--- controller/action/SearchAction.java
+++ controller/action/SearchAction.java
@@ -1,20 +1,15 @@
 package controller.action;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.HashMap;
+import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.CommentDao;
 import dao.WritingDao;
 import dto.Writing;
-import dto.Comment;
-import dto.PageDto;
 
 public class SearchAction implements Action {
 
@@ -22,31 +17,57 @@ public class SearchAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		request.setCharacterEncoding("UTF-8");
-		
+		// 검색 조건에 따른 파라미터 가져오기
 		String searchOption = request.getParameter("searchOption");
 		String subject = request.getParameter("subject");
 		String userId = request.getParameter("userId");
 		String content = request.getParameter("content");
-		
-		if(searchOption.equals("subject")) {
-			WritingDao wdao = WritingDao.getInstance();
-			List<Writing> slist = wdao.searchBySubject(subject);
-			request.setAttribute("slist", slist);
-		}else if(searchOption.equals("userId")) {
-			WritingDao wdao = WritingDao.getInstance();
-			List<Writing> dlist = wdao.searchById(userId);
-			request.setAttribute("dlist", dlist);
-		}else if(searchOption.equals("content")) {
-			WritingDao wdao = WritingDao.getInstance();
-			List<Writing> clist = wdao.searchByContent(content);
-			request.setAttribute("clist", clist);
+				
+		// 검색 주제 별 글 리스트 생성 후 요청 전달
+		if (searchOption.equals("subject")) {
+			if(!subject.equals("")) { 
+				WritingDao wdao = WritingDao.getInstance();
+				List<Writing> list = wdao.searchBySubject(subject);
+				request.setAttribute("list", list);
+			}else {
+				request.setAttribute("message", "제목을 입력해주세요");
+				request.setAttribute("url", request.getHeader("REFERER")); 
+				ActionForward foward = new ActionForward();
+				foward.isRedirect = false;
+				foward.url="/error/alert.jsp";
+				return foward;
+			}
+		} else if (searchOption.equals("userId")) {
+			if(!userId.equals("")) { 
+				WritingDao wdao = WritingDao.getInstance();
+				List<Writing> list = wdao.searchById(userId);
+				request.setAttribute("list", list);
+			}else {
+				request.setAttribute("message", "작성자를 입력해주세요");
+				request.setAttribute("url", request.getHeader("REFERER")); 
+				ActionForward foward = new ActionForward();
+				foward.isRedirect = false;
+				foward.url="/error/alert.jsp";
+				return foward;
+			}
+		} else if (searchOption.equals("content")) {
+			if(!content.equals("")) {
+				WritingDao wdao = WritingDao.getInstance();
+				List<Writing> list = wdao.searchByContent(content);
+				request.setAttribute("list", list);
+			}else {
+				request.setAttribute("message", "내용을 입력해주세요");
+				request.setAttribute("url", request.getHeader("REFERER")); 
+				ActionForward foward = new ActionForward();
+				foward.isRedirect = false;
+				foward.url="/error/alert.jsp";
+				return foward;
+			}
 		}
 		
 		ActionForward forward = new ActionForward();
 		forward.isRedirect = false;
-		forward.url="/view/search.jsp?searchOption"+searchOption+"&subject="+subject+"&userId="+userId+"&content="+content;
+		forward.setUrl("view/search.jsp");
 		return forward;
 	}
-
 }
