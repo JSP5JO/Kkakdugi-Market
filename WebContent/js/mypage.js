25e79bf2a0df6b7bb177cede6b1a83a516223828
Kimhyeri <hyelljs@gmail.com>
2021-10-22 21:15:11 +0900

    test

--- WebContent/js/mypage.js
+++ WebContent/js/mypage.js
@@ -1,6 +1,7 @@
+//css를 별도로 지정하기위해 ie 에서만 클래스 추가
+
 var agent = navigator.userAgent.toLowerCase();
 
 if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
 	$(".mlist_con").addClass("ie");
 }
-
