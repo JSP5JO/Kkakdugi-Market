2884b851077a137426285dd8d02c3e554e1afb0b
Kimhyeri <hyelljs@gmail.com>
2021-10-22 10:02:50 +0900

    test

--- dao/JjimDao.java
+++ dao/JjimDao.java
@@ -1,9 +1,8 @@
 package dao;
-import dto.Comment;
+
 import dto.Jjim;
 import dto.Writing;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -11,23 +10,18 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import mybatis.SqlSessionBean;
 
 public class JjimDao {
-	
-	SqlSessionFactory factory = SqlSessionBean.getSessionFactory();
+
+	private JjimDao() {}
+
+
 	private static JjimDao dao = new JjimDao();
-	
-	private JjimDao() { }
+
 	public static JjimDao getInstance() {
 		return dao;
 	}
 	
-	public List<Jjim> getList(Map<String, Integer> map){   
-			//key(변수명처럼 이해) String, value  는 int
-	List<Jjim> list = null;
-	SqlSession mapper = factory.openSession();
-	list = mapper.selectList("getList",map);
-	return list;
-	}
-	
+	SqlSessionFactory factory = SqlSessionBean.getSessionFactory();
+
 	public List<Jjim> selectById(String userId) {
 		List<Jjim> list = null;
 		SqlSession mapper = factory.openSession();
@@ -36,58 +30,11 @@ public class JjimDao {
 		return list;
 	}
 	
-	
-	
-	/*//idx로 한개 행 조회
-	public Writing getOne(int idx) {
+	public void userJjim(Jjim dto) {
 		SqlSession mapper = factory.openSession();
-		Writing dto = mapper.selectOne("selectByIdx", idx);  
-		mapper.close();
-		return dto;
-	}
-	
-	//테이블 데이터 행의 개수 조회
-	public int getCount() {
-		SqlSession mapper = factory.openSession();
-		int cnt = mapper.selectOne("getCount");  
-		mapper.close();     
-		return cnt;
-	}
-	
-	public void insert(Writing dto) {
-		SqlSession mapper = factory.openSession();
-		mapper.insert("freeboard.insert",dto);
+		mapper.insert("jjim.userJjim",dto);
 		mapper.commit();
 		mapper.close();
 	}
-	
-	public void update(Writing dto) {
-		SqlSession mapper = factory.openSession();
-		mapper.update("update",dto);
-		mapper.commit();
-		mapper.close();
-	}
-	
-	public int delete(Map<String,Object> map) {
-		SqlSession mapper = factory.openSession();
-		int n = mapper.delete("freeboard.delete",map);
-		mapper.commit();
-		mapper.close();
-		return n;
-	}
-	
-	public Writing passwordCheck(Map<String,Object> map) {
-		SqlSession mapper = factory.openSession();
-		Writing dto = mapper.selectOne("passwordCheck", map);
-		mapper.close();
-		return dto;
-	}
-	
-	public void readCount(int idx) {
-		SqlSession mapper =factory.openSession();
-		mapper.update("readCount", idx);
-		mapper.commit();
-		mapper.close();
-	}*/
-	
+
 }
