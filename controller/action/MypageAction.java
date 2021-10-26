b71b99f52b49cda08aa84c7d3c7aaed88af8c504
Kimhyeri <hyelljs@gmail.com>
2021-10-26 11:11:57 +0900

    test

--- controller/action/MypageAction.java
+++ controller/action/MypageAction.java
@@ -9,12 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.CommentDao;
-import dao.JjimDao;
 import dao.UsersDao;
 import dao.WritingDao;
 import dto.Writing;
 import dto.Comment;
-import dto.Jjim;
 import dto.SessionDto;
 import dto.Users;
 
@@ -23,40 +21,50 @@ public class MypageAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		request.setCharacterEncoding("UTF-8");
-//		String userId = request.getParameter("userId");
-//		String userId = "user1";
 		HttpSession session = request.getSession();
-		String[] users1 = String.valueOf(session.getAttribute("user")).split(", name=");
-		String[] users2 = String.valueOf(users1[0]).split(", id=");
-		String userId = String.valueOf(users2[1]);
-		WritingDao wdao = WritingDao.getInstance();
-		List<Writing> wlist = wdao.selectById(userId);
-		request.setAttribute("wlist", wlist);
-		
-		CommentDao cdao = CommentDao.getInstance();
-		List<Comment> clist = cdao.selectById(userId);
-		request.setAttribute("clist", clist);
-		
-		WritingDao jdao = WritingDao.getInstance();
-		List<Writing> jlist = jdao.selectById(userId);
-		request.setAttribute("jlist", jlist);
-		
-		/*UsersDao udao = UsersDao.getInstance();
-		List<Users> ulist = udao.proIdxInfo(userId);
-		request.setAttribute("ulist", ulist);*/
-		
-		WritingDao hdao = WritingDao.getInstance();
-		List<Writing> hlist = hdao.selectHelpById(userId);
-		request.setAttribute("hlist", hlist);
-		
-		request.setAttribute("userId", userId);
-		
-		ActionForward forward = new ActionForward();
-		forward.isRedirect = false;
-		forward.url="view/mypage.jsp?userId="+userId;
-		return forward;
+		SessionDto sdto = (SessionDto) session.getAttribute("user");
+
+		if (sdto == null) {
+			request.setAttribute("message", "로그인을 해주세요");
+			request.setAttribute("url", "index.do");
+			return new ActionForward(false, "error/alert.jsp");
+		} else {
+			String userId = sdto.getId();
+
+			// 내가 쓴 글 리스트 가져오기
+			WritingDao wdao = WritingDao.getInstance();
+			List<Writing> wlist = wdao.selectById(userId);
+			request.setAttribute("wlist", wlist);
+
+			// 내가 쓴 댓글 리스트 가졍괴
+			CommentDao cdao = CommentDao.getInstance();
+			List<Comment> clist = cdao.selectById(userId);
+			request.setAttribute("clist", clist);
+
+			// 내가 찜한 글 리스트 가져오기
+			List<Writing> jlist = wdao.selectJjimById(userId);
+			request.setAttribute("jlist", jlist);
+
+			// user 정보 가져오기
+			UsersDao udao = UsersDao.getInstance();
+			Users udto = new Users();
+			if(udao.proIdxInfo(userId)!=null) {
+			udto = udao.proIdxInfo(userId);
+			udto.setId(userId);
+			request.setAttribute("pro", udto);
+			}
+			// 내가 쓴 1:1상담 글 가져오기
+			WritingDao hdao = WritingDao.getInstance();
+			List<Writing> hlist = hdao.selectHelpById(userId);
+			request.setAttribute("hlist", hlist);
+
+			ActionForward forward = new ActionForward();
+			forward.isRedirect = false;
+			forward.url = "view/mypage.jsp?userId=" + userId;
+			return forward;
+		}
 	}
 
 }
